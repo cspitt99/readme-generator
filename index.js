9af937402e20e7e1b88bd9f9267af98ff8585f3d
cspitt99 <104328339+cspitt99@users.noreply.github.com>
2022-09-23 20:39:05 -0400

    implemented first question

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-// TODO: Include packages needed for this application
 const inquirer = require("inquirer")
+const {generateMarkdown} = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "title",
+        type: "input",
+        message: "What is the title?"
+    }
+];
+
+// Title, description, installation instructions, usage information, contribution guidelines, and test instructions, license, Github Username, Email Address
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    const answers = await inquirer.prompt(questions)
+    console.log(answers)
+}
 
 // Function call to initialize app
 init();
