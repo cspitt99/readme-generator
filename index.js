ab0f8ed21e271dc2d22d72543fe1ad56e486c1e9
cspitt99 <104328339+cspitt99@users.noreply.github.com>
2022-09-23 20:53:16 -0400

    added the questions

--- index.js
+++ index.js
@@ -7,7 +7,48 @@ const questions = [
         name: "title",
         type: "input",
         message: "What is the title?"
-    }
+    },
+    {
+        name: "description",
+        type: "input",
+        message: "What is the description?"
+    },
+    {
+        name: "installation",
+        type: "editor",
+        message: "What are the installation instructions?"
+    },
+    {
+        name: "usage",
+        type: "editor",
+        message: "What is the usage information?"
+    },
+    {
+        name: "contribution",
+        type: "input",
+        message: "What are the contribution guidelines?"
+    },
+    {
+        name: "test",
+        type: "input",
+        message: "What are the test instructions?"
+    },
+    {
+        name: "license",
+        type: "list",
+        message: "What is the license?",
+        choices: ["MIT","Apache-2.0","GPL-3.0"]
+    },
+    {
+        name: "username",
+        type: "input",
+        message: "What is your Github username?"
+    },
+    {
+        name: "email",
+        type: "input",
+        message: "What is your email?"
+    },
 ];
 
 // Title, description, installation instructions, usage information, contribution guidelines, and test instructions, license, Github Username, Email Address
